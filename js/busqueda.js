e38ecc2712809d128a7e4dd2c337b5e58b6510c5
unknown <porellanaj@est.ups.edu.ec>
2020-10-27 18:06:20 -0500

    Commit para buscar planetas

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,11 +1,19 @@
-var el = document.querySelector('#boton');
+var el = document.querySelector('#botonPersona');
+var el1 = document.querySelector('#botonPlaneta');
+
 if (el) {
   el.addEventListener('click', function(){
-    obtenerDatos();
+    datosPersona();
   });
 }
 
-function obtenerDatos() {
+if (el1) {
+  el1.addEventListener('click', function(){
+    datosPlaneta();
+  });
+}
+
+function datosPersona() {
 valor = document.getElementById("texto").value;
 
 if (valor == "") {
@@ -57,3 +65,58 @@ if (valor == "") {
     }
   }
 }
+
+function datosPlaneta() {
+  valor = document.getElementById("texto1").value;
+  
+  if (valor == "") {
+    let url1 = `https://swapi.dev/api/planets`;
+    const api = new XMLHttpRequest();
+    api.open('GET', url1, true);
+    api.send();
+    api.onreadystatechange = function (){
+      if (this.status == 200 && this.readyState == 4){
+        let datos1 = JSON.parse(this.responseText);
+        let planet = document.querySelector('#planet');
+        planet.innerHTML = '';
+        for (let i of datos1.results) {
+         planet.innerHTML += `<td>${i.name}</td>` +
+         `<td>${i.rotation_period}</td>` +
+         `<td>${i.orbital_peiod}</td>` +
+         `<td>${i.diameter}</td>` +
+         `<td>${i.climate}</td>` +
+         `<td>${i.gravity}</td>` +
+         `<td>${i.terrain}</td>` +
+         `<td>${i.surface_water}</td>` + 
+         `<td>${i.population}</td>`
+        }
+        }
+      }
+  }else if (valor != ""){
+  
+    let url = `https://swapi.dev/api/planets/?search=`+ valor;
+    const api = new XMLHttpRequest();
+    api.open('GET', url, true);
+    api.send();
+    api.onreadystatechange = function (){
+      if (this.status == 200 && this.readyState == 4){
+        let datos = JSON.parse(this.responseText);
+        console.log(datos);
+        let planet = document.querySelector('#planet');
+        planet.innerHTML = '';
+  
+        for (let i of datos.results) {
+          planet.innerHTML += `<td>${i.name}</td>`+
+         `<td>${i.rotation_period}</td>` +
+         `<td>${i.orbital_peiod}</td>` +
+         `<td>${i.diameter}</td>` +
+         `<td>${i.climate}</td>` +
+         `<td>${i.gravity}</td>` +
+         `<td>${i.terrain}</td>` +
+         `<td>${i.surface_water}</td>` + 
+         `<td>${i.population}</td>`
+         }
+        }
+      }
+    }
+  }
