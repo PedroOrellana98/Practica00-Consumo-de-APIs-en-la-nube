a18751a31d597eb4e2592c8f44ef7d9c2c60b56b
unknown <porellanaj@est.ups.edu.ec>
2020-10-27 16:52:23 -0500

    Commit busqueda por nombres

--- js/busqueda.js
+++ js/busqueda.js
@@ -6,9 +6,9 @@ if (el) {
 }
 
 function obtenerDatos() {
-valor = parseInt(document.getElementById("texto").value);
+valor = document.getElementById("texto").value;
 
-if (isNaN(valor)) {
+if (valor == "") {
   let url1 = `https://swapi.dev/api/people`;
   const api = new XMLHttpRequest();
   api.open('GET', url1, true);
@@ -30,32 +30,29 @@ if (isNaN(valor)) {
       }
       }
     }
-}else if (!isNaN(valor)){
+}else if (valor != ""){
 
-  let url = `https://swapi.dev/api/people/`+ valor + `/`;
+  let url = `https://swapi.dev/api/people/?search=`+ valor;
   const api = new XMLHttpRequest();
   api.open('GET', url, true);
   api.send();
   api.onreadystatechange = function (){
     if (this.status == 200 && this.readyState == 4){
       let datos = JSON.parse(this.responseText);
-      /*console.log(datos.result);
-      let film = document.querySelector('#film');
-      film.innerHTML = '';*/
+      console.log(datos);
       let people = document.querySelector('#people');
       people.innerHTML = '';
-      
-      people.innerHTML += `<td>${datos.name}</td>`+
-      `<td>${datos.height}</td>` +
-      `<td>${datos.mass}</td>` +
-      `<td>${datos.hair_color}</td>` +
-      `<td>${datos.skin_color}</td>` +
-      `<td>${datos.eye_color}</td>` +
-      `<td>${datos.birth_year}</td>` +
-      `<td>${datos.gender}</td>`
-      /*for (let i of datos.result) {
-       film.innerHTML += `<td>${i.title}</td>`
-      }*/
+
+      for (let i of datos.results) {
+        people.innerHTML += `<td>${i.name}</td>`+
+        `<td>${i.height}</td>` +
+        `<td>${i.mass}</td>` +
+        `<td>${i.hair_color}</td>` +
+        `<td>${i.skin_color}</td>` +
+        `<td>${i.eye_color}</td>` +
+        `<td>${i.birth_year}</td>` +
+        `<td>${i.gender}</td>`
+       }
       }
     }
   }
